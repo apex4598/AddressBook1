5c7a0330008325d515e5cf69f4f07b1f796337de
Apex4598 <login2ajit@gmail.com>
2022-06-10 08:44:13 +0530

    Address Book

---
+++ src/com/bridgelabz/AddressMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address Book ");
+        AddressBook person = new AddressBook();
+
+        person.add();
+           }
+}
